c619854cb8226b94a0925fc2a3134bc5a4f53119
Mohit Vishwakarma <mohitvishwakarma681@gmail.com>
2024-01-11 14:01:21 +0530

    learned props and props destructuring

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const Header = () => {
   return (
     <nav className="nav">
       <img
-        src="https://image.similarpng.com/very-thumbnail/2021/09/Good-food-logo-design-on-transparent-background-PNG.png"
+        src="https://cdn.vectorstock.com/i/preview-1x/73/82/food-logo-vector-38377382.jpg"
         className="logo"
       ></img>
       <ul className="nav-items">
@@ -36,10 +36,21 @@ const Header = () => {
   );
 };
 
-const RestaurantCard = () => {
+const RestaurantCard = (props) => {
+  const { resName, cuisine, stars, time } = props;
   return (
-    <div className="res-card" style={{backgroundColor:'#f0f0f0'}}>
-      <h2>Meghana Foods</h2>
+    <div className="res-card" style={{ backgroundColor: "#f0f0f0" }}>
+      <img
+        className="res-logo"
+        src="https://www.gundersenhealth.org/sites/default/files/styles/card_medium_4_3_600_450/public/eat-balance-your-plate.jpg.webp?itok=UGYjLYzZ"
+        alt="res-logo"
+      ></img>
+      <div className="res-content">
+        <h2>{resName}</h2>
+        <p>{cuisine} </p>
+        <p>{stars}</p>
+        <p>{time}</p>
+      </div>
     </div>
   );
 };
@@ -49,7 +60,30 @@ const Body = () => {
     <div className="body">
       <div className="search">Search</div>
       <div className="res-container">
-        <RestaurantCard />
+        <RestaurantCard
+          resName="Meghana Foods"
+          cuisine="Burger with Love & Chilli"
+          stars="4"
+          time="30 mins"
+        />
+        <RestaurantCard
+          resName="KFC"
+          cuisine="Fully Loaded pizza with cheeze"
+          stars="4.5"
+          time="28 mins"
+        />
+        <RestaurantCard
+          resName="Khana Wala"
+          cuisine="Allo ki sabji "
+          stars="5"
+          time="20 mins"
+        />
+        <RestaurantCard
+          resName="Gutam Fast Foods"
+          cuisine="chilli and Garlic Tadka Daal with Rice"
+          stars="3"
+          time="25 mins"
+        />
       </div>
     </div>
   );
