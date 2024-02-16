e7a76196e7bf90a7501163fda38305065a67f1a6
Mohit Vishwakarma <mohitvishwakarma681@gmail.com>
2024-02-16 19:23:39 +0530

    refactored RestaurantCard UI

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,20 +3,22 @@ import Shimmer from "./Shimmer.js";
 import { Link } from "react-router-dom";
 import useResList from "../utils/useResList.js";
 
-
 const Body = () => {
-  const sectionListFromApiData = useResList();
+  const sectionListFromApi = useResList();
 
-  if(!sectionListFromApiData.length) return(<Shimmer/>) ;
+  if (!sectionListFromApi.length) return <Shimmer />;
 
-  const workingResList = sectionListFromApiData[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
+  const workingResList =
+    sectionListFromApi[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
 
   const RestaurantCardPromoted = WithPromotedLabel(RestaurantCard);
 
   return (
-    <div className="mt-3">
+    <div className="mx-[10%]">
       <hr className="my-2" />
-
+      <h3 className="font-bold text-2xl ms-8">
+        Restaurants with online food delivery in Jabalpur
+      </h3>
       <div className="ms-5 flex flex-wrap justify-start ">
         {workingResList?.map((restaurant) => {
           return (
--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -5,39 +5,48 @@ const RestaurantCard = (props) => {
     props.resData?.info; //Optional chaining
 
   return (
-    <div data-testid="resCard" className="w-52 h-[400px] m-4 border bg-gray-200 p-2 rounded-lg">
-      <img
-        className="h-48 w-48 object-cover rounded-lg"
-        src={res_logo_cdn + cloudinaryImageId}
-        alt="res-logo"
-      ></img>{" "}
-      <div className="mt-2">
-        <h2 className="font-bold bg-gray-200 text-lg mb-2">{name}</h2>
-        <p>{cuisines.length>3?cuisines.slice(0,2).join(", "):cuisines.join(", ")} </p>
-        <span>Rating: </span><span>{avgRating}</span>
-        <p>time: {sla.deliveryTime} mins</p>
-        <p>Price:{costForTwo}</p>
+    <div data-testid="resCard" className="w-72 h-72 m-4 rounded-lg ">
+      <div className="hover:p-2 transition-all">
+        <img
+          className="h-48 w-full object-cover rounded-2xl"
+          src={res_logo_cdn + cloudinaryImageId}
+          alt="res-logo"
+        ></img>
+        <div className="pt-2 ps-4">
+          <h2 className="font-bold text-gray-600 text-xl mb-1">
+            {name.length > 22 ? name.slice(0, 22) + "..." : name}
+          </h2>
+          <span>⭐</span>
+          <span className="text-gray-600 font-bold">
+            {avgRating} • {sla.deliveryTime} mins
+          </span>
+          <p className="text-md text-gray-400">
+            {cuisines.length > 3
+              ? cuisines.slice(0, 2).join(", ") + "..."
+              : cuisines.join(", ")}
+          </p>
+          {/* <p>Price:{costForTwo}</p> */}
+        </div>
       </div>
     </div>
   );
 };
 
- //Higher Order component
+//Higher Order component
 
- //input - RestaurantCard => RestaurantCardPromoted (output)
+//input - RestaurantCard => RestaurantCardPromoted (output)
 
-  
- export const WithPromotedLabel = (RestaurantCard)=>{
-
-    return (props)=>{
-
-      return(
-        <div>
-          <label className="absolute bg-green-500 text-white py-1 px-2 rounded-md">veg</label>
-          <RestaurantCard {...props}/> 
-        </div>
-      )
-    }
-  }
+export const WithPromotedLabel = (RestaurantCard) => {
+  return (props) => {
+    return (
+      <div>
+        <label className="absolute bg-green-500 text-white py-1 px-2 rounded-md">
+          veg
+        </label>
+        <RestaurantCard {...props} />
+      </div>
+    );
+  };
+};
 
 export default RestaurantCard;
